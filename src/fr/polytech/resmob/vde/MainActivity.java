3014ea55a880981487e022540ef0bef54ec3c47c
sylvain <goleem@gmail.com>
2012-11-27 21:53:45 +0100

    Ajout de la gestion de préférences.
    Notamment pour l'adresse du serveur.

--- src/fr/polytech/resmob/vde/MainActivity.java
+++ src/fr/polytech/resmob/vde/MainActivity.java
@@ -2,11 +2,14 @@ package fr.polytech.resmob.vde;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,8 +17,12 @@ public class MainActivity extends Activity {
 	
 	HttpHandler httpHandler;
 	
-	// Boutons utilisé pour lancer l'activité d'envoi de nouvel article
+	// Bouton utilisé pour lancer l'activité d'envoi de nouvel article
 	private Button postButton;
+	private Button prefButton;
+	
+	// Préférences
+	private String url_server;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,7 @@ public class MainActivity extends Activity {
 		
 		context = this;
 		
-		httpHandler = new HttpHandler(new String(R.string.url_server + ""));
+		httpHandler = new HttpHandler(new String(R.string.url_server_default + ""));
 		
 		postButton = (Button) findViewById(R.id.buttonPost);
 		postButton.setOnClickListener(new OnClickListener() {
@@ -36,8 +43,28 @@ public class MainActivity extends Activity {
 			}
 			
 		});
+		
+		prefButton = (Button) findViewById(R.id.buttonPref);
+		prefButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Lancement de l'activité "envoi d'article"
+				Intent prefIntent = new Intent(context, PreferencesActivity.class);
+				startActivity(prefIntent);
+			}
+			
+		});
 	}
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// On récupère les préférences dans le onStart() pour que le changement d'options
+		// soit pris en compte quand on revient de l'activité préférences.
+		getPrefs();
+		Toast.makeText(context, "URL serveur = " + url_server, Toast.LENGTH_SHORT).show();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -45,4 +72,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	// Permet d'enregistrer les préférences dans les attributs de la classe
+	private void getPrefs() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		url_server = prefs.getString("serverPref", getResources().getString(R.string.url_server_default));
+	}
 }
